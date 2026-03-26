ed5448472b6cfa67e9e27ce0998b82cf91b59ecd
weiyaliu <343445839@tdsb.ca>
2026-03-26 08:39:41 -0400

    jkascj

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -8,7 +8,7 @@ public class PracticeProblem {
 	public static String evenOrOdd (int num) {
 	  if (num == 0) {
 	      return "Zero";
-	  } else if (num % 2 += 0) {
+	  } else if (num % 2 == 0) {
 	      return "Even";
 	  } else {
 	      return "Odd";
